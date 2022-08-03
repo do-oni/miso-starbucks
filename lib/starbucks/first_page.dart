ea80b0a14eb48d3726602edcfc20d6a52de533ca
do-oni <dooniseo@gmail.com>
2022-08-03 13:39:55 +0900

    Add: first_page

--- lib/starbucks/first_page.dart
+++ lib/starbucks/first_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:miso_starbucks/starbucks/starbucks.dart';
 
 class StarbucksFirstPage extends StatelessWidget {
   const StarbucksFirstPage({Key? key}) : super(key: key);
@@ -41,8 +42,353 @@ class StarbucksFirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text("Starbucks 첫 번째 페이지"),
+
+      // 우측 하단에 Deliverys 아이콘을 띄위 위해 Stack으로 구현
+      body: Stack(
+        children: [
+          // Sliver 위젯들을 이용하려면 CustomScrollView를 사용해야 합니다.
+          CustomScrollView(
+            slivers: [
+              // 스크롤시 배경이 사라지게 만들려면 SliverAppBar 위젯을 사용하면 됩니다.
+              // SliverAppBar에 대한 자세한 내용은 아래 링크를 참고해 주세요.
+              // https://api.flutter.dev/flutter/material/SliverAppBar-class.html
+              SliverAppBar(
+                automaticallyImplyLeading: false, // 뒤로가기 버튼 숨기기
+                pinned: true, // 스크롤시 bottom 영역을 화면 상단에 남길지 여부
+                snap: false, // 중간에 멈출 때 자동으로 AppBar를 펼쳐서 배경을 모두 보여줄지
+                floating: true, // AppBar를 화면에 띄울지, 아니면 컬럼처럼 최 상단에 놓을지
+                expandedHeight: 252, // 최대 확장되었을 떄 높이
+                backgroundColor: Colors.white,
+
+                // 스크롤시 사라지는 영역
+                flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.pin,
+                  background: Stack(
+                    children: [
+                      // 백그라운드 이미지
+                      Positioned.fill(
+                        bottom: 60,
+                        child: Image.network(
+                          backImg,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+
+                      // 배경 위 위젯
+                      Positioned(
+                        left: 24,
+                        right: 24,
+                        bottom: 60,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "한 해의 마무리,\n수고 많았어요💖",
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 32),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                // LinearProgressIndicator가 끝없이 길어지지 않도록 Column의 가로 길이를 Row의 남은 자리만큼만 차지하도록 만들어줌
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "11 ★ until next Reward",
+                                        style: TextStyle(
+                                          color: starbucksAccentColor,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(height: 16),
+
+                                      // LinearProgressIndicator는 각져있는데, 둥글게 보이도록 모서리를 잘라냄
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10),
+                                        ),
+                                        child: LinearProgressIndicator(
+                                          backgroundColor:
+                                              Colors.grey.withOpacity(0.2),
+                                          value: 0.083,
+                                          minHeight: 10,
+                                          valueColor:
+                                              AlwaysStoppedAnimation<Color>(
+                                            starbucksAccentColor,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(width: 16),
+                                RichText(
+                                  textAlign: TextAlign.center,
+                                  text: TextSpan(
+                                    // 공통 스타일
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      color: Colors
+                                          .black, // RichText는 기본이 흰색이라 안보임
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: "1",
+                                        style: TextStyle(
+                                          fontSize: 38,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: " / ",
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: "12 ★",
+                                        style: TextStyle(
+                                          color: starbucksAccentColor,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // 스크롤시 남아있는 영역
+                // SliverAppBar의 bottom은 PreferredSize 위젯으로 시작해야만 함.
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(52), // 영역 높이
+                  child: Container(
+                    height: 52,
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24,
+                        right: 12,
+                      ),
+                      child: Row(
+                        children: [
+                          // What's New
+                          GestureDetector(
+                            onTap: () => print("What's New 클릭됨"),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.mail_outline,
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  "What's New",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 32),
+
+                          // Coupon
+                          GestureDetector(
+                            onTap: () => print("Coupon 클릭됨"),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.confirmation_num_outlined,
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  "Coupon",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Spacer(),
+
+                          // Alarm
+                          Stack(
+                            children: [
+                              Icon(
+                                Icons.notification_add_outlined,
+                                color: Colors.grey,
+                                size: 32,
+                              ),
+                              Positioned(
+                                right: 2,
+                                top: 2,
+                                child: CircleAvatar(
+                                  radius: 5,
+                                  backgroundColor: starbucksPrimaryColor,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              // CustomScrollView 안에서는 모든 첫 번째 위젯이 Sliver로 구현 되어야 함
+              // SliverToBoxAdapter는 Container와 같은 위젯이라고 보면 됨
+              SliverToBoxAdapter(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Frequency
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 18,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          frequencyImg,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32),
+
+                    // 추천 메뉴 Title
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          // 공통 스타일
+                          style: TextStyle(
+                            fontSize: 28,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          children: [
+                            TextSpan(
+                                text: "이찬호",
+                                style: TextStyle(
+                                  color: starbucksAccentColor,
+                                )),
+                            TextSpan(text: "님을 위한 추천 메뉴"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32),
+
+                    // 추천 메뉴 horizontal list view
+                    SizedBox(
+                      height: 150, // 높이를 가져야 ListView를 Column 안에 넣을 수 있음
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal, // 횡 스크롤
+                        itemCount: 100,
+                        itemBuilder: (context, index) {
+                          final menu =
+                              recommendMenu[index % recommendMenu.length];
+                          final name = menu["name"] ?? "이름";
+                          final imgUrl = menu["imgUrl"] ?? "";
+                          return SizedBox(
+                            width: 128,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                CircleAvatar(
+                                  radius: 52,
+                                  // circleAvatar 배경에 맞춰서 동그랗게 이미지 넣는 방법
+                                  backgroundImage: NetworkImage(imgUrl),
+                                  backgroundColor: Colors.transparent,
+                                ),
+                                SizedBox(height: 6),
+                                Text(
+                                  name,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+
+                    // Event
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 18,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          eventImg,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          // Deliverys
+          Positioned(
+            bottom: 18,
+            right: 24,
+            child: GestureDetector(
+              onTap: () => print("Deliverys 클릭됨"),
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 12,
+                ),
+                decoration: BoxDecoration(
+                  color: starbucksPrimaryColor,
+                  borderRadius: BorderRadius.circular(64),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      "Deliverys",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Icon(
+                      Icons.pedal_bike_outlined,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
