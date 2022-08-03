3161a6d567a850700cc59e233d47fa4cf9f7c9b9
do-oni <dooniseo@gmail.com>
2022-08-03 11:14:04 +0900

    Add: third_page

--- lib/starbucks/third_page.dart
+++ lib/starbucks/third_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:miso_starbucks/starbucks/starbucks.dart';
 
 class StarbucksThirdPage extends StatelessWidget {
   const StarbucksThirdPage({Key? key}) : super(key: key);
@@ -29,9 +30,102 @@ class StarbucksThirdPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Starbucks 세 번째 페이지"),
+    // TabBar controller를 직접 TabBar에 넣어줄 수도 있고, 아래와 같이 위젯으로 감싸줄 수도 있음.
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Order",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.white,
+          actions: [
+            IconButton(
+              onPressed: () => print("Pay 우측 상단 아이콘 클릭됨"),
+              icon: Icon(Icons.search_outlined, color: Colors.grey),
+            ),
+          ],
+
+          // AppBar 하단에 TobBar를 만들어줌.
+          bottom: TabBar(
+            isScrollable: false,
+            indicatorColor: starbucksPrimaryColor,
+            indicatorWeight: 4,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(text: "전체 메뉴"),
+              Tab(text: "나만의 메뉴"),
+              Tab(text: "🎂 홀케이크"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            // 전체 메뉴
+            ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                final item = menu[index % menu.length];
+                final ko = item["ko"] ?? "제목";
+                final en = item["en"] ?? "title";
+                final imgUrl = item["imgUrl"] ?? "";
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 21,
+                  ),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 52,
+                        // circleAvaTar 배경에 맞춰서 동그랗게 이미지 넣는 방법
+                        backgroundImage: NetworkImage(imgUrl),
+                        backgroundColor: Colors.transparent,
+                      ),
+                      SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            ko,
+                            style: TextStyle(
+                              fontSize: 21,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 6),
+                          Text(
+                            en,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+
+            // 나만의 메뉴
+            Center(child: Text("나만의 메뉴")),
+
+            // 홀케이크 예약
+            Center(child: Text("홀케이크 예약")),
+          ],
+        ),
       ),
     );
   }
